5e71fe2dc8698d53e2d3f403ba789f4b68069cea
svetlanacodrean <47977916+svetlanacodrean@users.noreply.github.com>
2020-09-17 13:58:25 +0300

    read 1000 sudokus

--- SudokuSAT/main.py
+++ SudokuSAT/main.py
@@ -29,38 +29,75 @@ def load_txt(file): # transform a sudoku into cnf
     return cnf
 
 
-sudoku = load_txt('sat_tests/sudoku_txt/sudoku1.txt')
-# sudoku = load_dimacs('sat_tests/sudoku_dimacs/sudoku2')
+def print_sudoku_grid(sol):
+    for i in range(0, 81):
+        print(sol[i] % 10, end=' ')
+        if (i + 1) % 9 == 0:
+            print('\n', end='')
+
+
+def write_output(sol):
+    output = open('file.out', 'w')
+    for element in sol:
+        output.write(str(element) + ' 0 \n')
+    output.close()
+
+
+def check_if_missing(sol):
+    lag = 0
+    for i in range(0, len(sol)):
+        cell = i + 11 + (i // 9)
+        if sol[i - lag] // 10 != cell:
+            print('Missing: ', end='')
+            print(cell)
+            lag = lag + 1
+
+
+def load_many_sudokus(file, rules):
+    f = open(file, 'r')
+    data = f.read()
+    f.close()
+    lines = data.split("\n")
+    for line in lines:
+        cnf = []
+        for i in range(1, 10):
+            for j in range(1, 10):
+                if line[0] != '.':
+                    cnf.append([i * 100 + j * 10 + int(line[0])])
+                line = line[1:]
+        cnf += rules
+        start = time.time()
+        sol = []
+        print(sat_solver.dp(cnf, sol))
+        sol.sort()
+        print('Sudoku solution: ', sol)
+        print('Solution length: ', len(sol))
+        end = time.time()
+        print('Time: ', end - start)
+        print_sudoku_grid(sol)
+
+
 rules = load_dimacs('sudoku-rules.txt')
-cnf = sudoku + rules
+load_many_sudokus('sat_tests/sudoku_txt/1000 sudokus.txt', rules)
 
 
-start = time.time()
-sol = []
-print(sat_solver.dpll(cnf, sol))
-sol.sort()
-print('Sudoku solution: ', sol)
-print('Solution length: ', len(sol)) # length > 81 means there are multiple solutions
-end = time.time()
-print('Time: ', end - start)
+# sudoku = load_txt('sat_tests/sudoku_txt/sudoku5.txt')
+# # sudoku = load_dimacs('sat_tests/sudoku_dimacs/sudoku2')
+# rules = load_dimacs('sudoku-rules.txt')
+# cnf = sudoku + rules
 
 
-lag = 0
-for i in range(0, len(sol)):
-    cell = i + 11 + (i // 9)
-    if sol[i - lag] // 10 != cell:
-        print('Missing: ', end='')
-        print(cell)
-        lag = lag + 1
-
-
-for i in range(0, 81):
-    print(sol[i]%10, end=' ')
-    if (i+1) % 9 == 0:
-        print('\n', end='')
-
-
-output = open('file.out', 'w')
-for element in sol:
-    output.write(str(element) + ' 0 \n')
-output.close()
+# start = time.time()
+# sol = []
+# print(sat_solver.dp(cnf, sol))
+# sol.sort()
+# print('Sudoku solution: ', sol)
+# print('Solution length: ', len(sol)) # length > 81 means there are multiple solutions
+# end = time.time()
+# print('Time: ', end - start)
+#
+#
+# check_if_missing(sol)
+#
+# print_sudoku_grid(sol)
+# write_output(sol)
