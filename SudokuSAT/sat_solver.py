a79ba5954c453b34f8bde5bde2d13111ba4e30d1
svetlanacodrean <47977916+svetlanacodrean@users.noreply.github.com>
2020-09-17 13:59:10 +0300

    Update sat_solver.py

--- SudokuSAT/sat_solver.py
+++ SudokuSAT/sat_solver.py
@@ -2,7 +2,7 @@ import random
 from statistics import mode
 
 
-def dpll(cnf, solution):
+def dp(cnf, solution):
     if not cnf:
         return True
     if [] in cnf:
@@ -11,17 +11,19 @@ def dpll(cnf, solution):
         if len(clause) == 1:  # unit propagation
             if clause[0] > 0 and clause[0] not in solution:
                 solution.append(clause[0])
-            return dpll(reduced(cnf, clause[0]), solution)
+            # cnf = reduced(cnf, clause[0]) it is slower like this
+            return dp(reduced(cnf, clause[0]), solution)
     # literals = literals_list(cnf)  it is slower with this
     # for literal in literals:
     #     if is_pure(literals, literal):
     #         return dpll(reduced(cnf, literal))
 
-    # chosen = random_select(cnf)
-    chosen = most_common(cnf) # first heuristic
+    # chosen = cnf_to_flat_list(cnf)[0]
+    # chosen = random_select(cnf) # first heuristic
+    chosen = most_common(cnf) # second heuristic -> DLIS
 
     sol_copy = [x for x in solution]
-    a = dpll(reduced(cnf, chosen), sol_copy)
+    a = dp(reduced(cnf, chosen), sol_copy)
     if a:
         for x in sol_copy:
             if x not in solution:
@@ -30,7 +32,7 @@ def dpll(cnf, solution):
             solution.append(chosen)
         return a
     sol_copy2 = [x for x in solution]
-    b = dpll(reduced(cnf, -chosen), sol_copy2)
+    b = dp(reduced(cnf, -chosen), sol_copy2)
     if b:
         for x in sol_copy2:
             if x not in solution:
@@ -40,11 +42,11 @@ def dpll(cnf, solution):
     return b
 
 
-def reduced(cnf, var): # returns a new cnf without clauses that include var and without -var
+def reduced(cnf, literal): # returns a new cnf without clauses that include var and without -var
     new_cnf = []
     for clause in cnf:
-        if var not in clause:
-            new_cnf.append([x for x in clause if x != -var])
+        if literal not in clause:
+            new_cnf.append([x for x in clause if x != -literal])
     return new_cnf
 
 
