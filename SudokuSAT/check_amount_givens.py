4c3eaa02853722fc3360fed275282dd9fcf9c576
Ready4GitHub <radhikesh.ramautarsing@gmail.com>
2020-09-21 20:29:42 +0200

    Percentage of givens

--- SudokuSAT/check_amount_givens.py
+++ SudokuSAT/check_amount_givens.py
@@ -1,20 +1,32 @@
-sudoku_amount = len(open('sat_tests/sudoku_txt/1000 sudokus.txt').readlines())
-print(sudoku_amount)
-linebreak_amount = sudoku_amount - 1
+def determine_sudoku_file(version):
+    if version == 4:
+        file = open("sat_tests/sudoku_txt/4x4.txt", "r") #open file in read mode
+        sudoku_amount = len(open('sat_tests/sudoku_txt/4x4.txt').readlines())
+        return check_sudoku(file, sudoku_amount, version)
+    elif version == 9:
+        file = open("sat_tests/sudoku_txt/1000 sudokus.txt", "r") #open file in read mode
+        sudoku_amount = len(open('sat_tests/sudoku_txt/1000 sudokus.txt').readlines())
+        return check_sudoku(file, sudoku_amount, version)
 
-#open file in read mode
-file = open("sat_tests/sudoku_txt/1000 sudokus.txt", "r")
 
-#read the content of file and replace the dots with nothing
-data = file.read().replace(".","")
+def check_sudoku(file, sudoku_amount, version):
+    print('Amount of sudokus in this file :', sudoku_amount)
+    linebreak_amount = sudoku_amount - 1
 
-#get the length of the data
-number_of_characters = len(data)-linebreak_amount
+    #read the content of file and replace the dots with nothing
+    data = file.read().replace(".","")
 
-average_given = number_of_characters/sudoku_amount
-percentage_given = (average_given/81)*100
+    #get the length of the data
+    number_of_givens = len(data)-linebreak_amount
 
-print('Number of characters in text file :', number_of_characters)
-print('Average of givens per Sudoku :', average_given)
-print('Amount percentage of givens: ', percentage_given)
+    average_given = number_of_givens/sudoku_amount
+    percentage_given = (average_given/(version*version))*100
+
+    print('Total amount of givens in text file :', number_of_givens)
+    print('Average of givens per Sudoku :', average_given)
+    print('Percentage of givens: ', percentage_given)
+
+
+pick_sudoku = input("Choose the sudoku version you want to analyze (4 or 9): ")
+determine_sudoku_file(int(pick_sudoku))
 
