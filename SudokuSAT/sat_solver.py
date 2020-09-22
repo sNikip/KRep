e9084dfb965dcd2fb789ef6de34268bd5ec89ccf
svetlanacodrean <47977916+svetlanacodrean@users.noreply.github.com>
2020-09-22 20:16:34 +0300

    Update sat_solver.py

--- SudokuSAT/sat_solver.py
+++ SudokuSAT/sat_solver.py
@@ -1,31 +1,48 @@
 import random
 from statistics import mode
+import numpy as np
 
+nrofSplits = 0
+nrofBacktracks = 0
+heur = ''
 
-def dp(cnf, solution):
+def dp(cnf, solution, h):
+    global nrofSplits
+    global nrofBacktracks
+    global nrofSplits_tosave
+    global nrofBacktracks_tosave
     if not cnf:
+        nrofSplits_tosave = nrofSplits
+        nrofBacktracks_tosave = nrofBacktracks
+        nrofSplits = 0
+        nrofBacktracks = 0
         return True
     if [] in cnf:
+        nrofBacktracks += 1
         return False
     for clause in cnf:
         if len(clause) == 1:  # unit propagation
             if clause[0] > 0 and clause[0] not in solution:
                 solution.append(clause[0])
-            # cnf = reduced(cnf, clause[0]) it is slower like this
-            return dp(reduced(cnf, clause[0]), solution)
-    # literals = literals_list(cnf)  it is slower with this
-    # for literal in literals:
-    #     if is_pure(literals, literal):
-    #         return dpll(reduced(cnf, literal))
+            # cnf = reduced(cnf, clause[0])
+            return dp(reduced(cnf, clause[0]), solution, h)
 
-    # chosen = cnf[0][0]
-    chosen = random_select(cnf) # first heuristic
-    #chosen = most_common(cnf) # second heuristic -> DLIS
+    if h == 0:
+        chosen = cnf[0][0]
+    elif h == 1:
+        chosen = random_select(cnf) # first heuristic
+    elif h == 2:
+        chosen = most_common(cnf) # second heuristic -> DLIS
+    elif h == 3:
+        chosen = dlcs(cnf)
+    elif h == 4:
+        chosen = min_heur(cnf)
+
+    nrofSplits += 1
 
     sol_copy = [x for x in solution]
-    a = dp(reduced(cnf, chosen), sol_copy)
+    a = dp(reduced(cnf, chosen), sol_copy, h)
     if a:
-        # print('a ', chosen)
         for x in sol_copy:
             if x not in solution:
                 solution.append(x)
@@ -33,9 +50,8 @@ def dp(cnf, solution):
             solution.append(chosen)
         return a
     sol_copy2 = [x for x in solution]
-    b = dp(reduced(cnf, -chosen), sol_copy2)
+    b = dp(reduced(cnf, -chosen), sol_copy2, h)
     if b:
-        # print('b ', -chosen)
         for x in sol_copy2:
             if x not in solution:
                 solution.append(x)
@@ -64,6 +80,8 @@ def random_select(cnf):
     if not cnf:
         return 0
     cnf_flat_list = cnf_to_flat_list(cnf)
+    global heur
+    heur = 'Random'
     return random.choice(cnf_flat_list)
 
 
@@ -71,23 +89,43 @@ def most_common(cnf): # first heuristics: maximum individual sum
     if not cnf:
         return 0
     cnf_flat_list = cnf_to_flat_list(cnf)
+    global heur
+    heur = 'DLIS'
     return mode(cnf_flat_list)
 
 
-# this was for pure literal
-# def is_pure(literals, literal):
-#     if not literals:
-#         return False
-#     if -literal in literals:
-#         return False
-#     return True
+def dlcs(cnf): # first heuristics: maximum combined sum
+    if not cnf:
+        return 0
+    cnf_flat_list = cnf_to_flat_list(cnf)
+    global heur
+    heur = 'DLCS'
+    count_pos = 0
+    count_neg = 0
+    m = mode([abs(x) for x in cnf_flat_list])
+    for el in cnf_flat_list:
+        if el == m:
+            count_pos += 1
+        elif el == -m:
+            count_neg += 1
+    if count_pos > count_neg:
+        return m
+    return -m
 
 
-# this was for pure literal
-# def literals_list(cnf):
-#     literals = []
-#     for clause in cnf:
-#         for literal in clause:
-#             if literal not in literals:
-#                 literals.append(literal)
-#     return literals
+def min_heur(cnf):
+    if not cnf:
+        return 0
+    cnf_flat_list = cnf_to_flat_list(cnf)
+    global heur
+    heur = 'Min Heur'
+    return min(cnf_flat_list)
+
+def pdlis(cnf):
+    if not cnf:
+        return 0
+    cnf_flat_list = cnf_to_flat_list(cnf)
+    values = [cnf_flat_list[key] for key in cnf_flat_list.keys()]
+    prob = [(value / sum(values)) for value in values]
+
+    return np.random.choice(cnf_flat_list, size = 1, p=prob)[0]
